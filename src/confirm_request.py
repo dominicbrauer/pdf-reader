2297f6d0e826cfd1fd84221da9c7e97a3b84b075
Dominic Brauer <dominicbrauer06@gmail.com>
2025-05-01 21:44:31 +0200

    Change file ending recognition

--- src/confirm_request.py
+++ src/confirm_request.py
@@ -1,8 +1,8 @@
 def confirmRequest(userConfig):
-  if userConfig["pdf_location"][-4:] != ".pdf" or userConfig["pdf_location"] == "":
+  if userConfig["pdf_location"][-4:].lower() != ".pdf" or userConfig["pdf_location"] == "":
     return False
   
-  if userConfig["csv_location"][-4:] != ".csv" or userConfig["csv_location"] == "":
+  if userConfig["csv_location"][-4:].lower() != ".csv" or userConfig["csv_location"] == "":
     return False
   
   return True
