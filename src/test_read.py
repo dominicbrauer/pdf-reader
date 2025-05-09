5073afae620c569a071c59d0fd7281ee5f2ae878
Dominic Brauer <dominicbrauer06@gmail.com>
2025-05-09 22:26:49 +0200

    Add test reading

--- src/test_read.py
+++ src/test_read.py
@@ -1,10 +1,15 @@
 import pdfplumber
 
+
+def removeSpaces():
+  pass
+
+
 def parsePDF():
   """Parses the given PDF's tables"""
 
   with pdfplumber.open("C:\\Users\\Dominic\\Desktop\\Scan_20250505_162506.pdf") as pdf:
-    
+
     data = []
 
     for pageIdx, page in enumerate(pdf.pages):
@@ -12,12 +17,17 @@ def parsePDF():
         "horizontal_strategy": "text",
         "vertical_strategy": "explicit",
         "text_tolerance": 1,
-        "explicit_vertical_lines": [40, 100, 410, 550]
+        "explicit_vertical_lines": [40, 75, 115, 420, 535, 585]
       })
-# [410, 550]
-      data.append(table)
-    
-    return data[0]
+
+      for idx, row in enumerate(table):
+        if row[0] == "Bu-Tag":
+          for _ in range(0, idx):
+            table.pop(0)
+          data.append(table)
+          break
+
+    return data[3]
 
     #   data[pageIdx] = []
 
